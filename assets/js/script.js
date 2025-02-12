bcd9c201b1899720a0d75c701f7f95c26b248539
ad <62818208+0adri3n@users.noreply.github.com>
2025-02-12 14:34:26 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -99,11 +99,11 @@ function togglePreview() {
   if (!isPreviewMode) {
     outputDiv.style.transform = "translateX(-100%)";
     editorDiv.style.display = "none";
-    eyeimgDiv.src = "static/assets/img/eye.png";
+    eyeimgDiv.src = "assets/img/eye.png";
   } else {
     editorDiv.style.display = "block";
     outputDiv.style.transform = "translateX(0%)";
-    eyeimgDiv.src = "static/assets/img/eyeclose.png";
+    eyeimgDiv.src = "assets/img/eyeclose.png";
   }
 
   isPreviewMode = !isPreviewMode;
@@ -130,7 +130,7 @@ function WidthChange(mq) {
     var eyeimgDiv = document.getElementById("eyeimg");
     outputDiv.style.transform = "translateX(0%)";
     editorDiv.style.display = "block";
-    eyeimgDiv.src = "static/assets/img/eyeclose.png";
+    eyeimgDiv.src = "assets/img/eyeclose.png";
   }
 }
 
